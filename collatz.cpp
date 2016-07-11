01fe99932ce26b192f2b8691f860691ca9e07f6c
Edwin Vargas <onix.edwin18@gmail.com>
2016-07-10 21:36:44 -0430

    Some changes (part II)

--- collatz.cpp
+++ collatz.cpp
@@ -2,6 +2,22 @@
 #include <iostream>
 using namespace std;
 
+bool isprime(int number){
+
+    if(number < 2) return false;
+    if(number == 2) return true;
+    if(number % 2 == 0) return false;
+    
+	for(int i = 3; (i*i) <= number; i += 2){
+    	if(number % i == 0 ) 
+			return false;
+    }
+    
+    return true;
+}
+
+
+
 void collatz(long int number)
 {
 	fstream file;
@@ -13,8 +29,11 @@ void collatz(long int number)
 		return;
 	}
 	
-	file << "Number to analize: " << number << endl << endl;
-	file << "Iteration\tPar\t\tImpar" << endl;
+	file << "Número a analizar: " << number << endl << endl;
+	file << "Etiquetas: " << endl;
+	file << "Numero primo -" << endl;
+	file << endl << endl;
+	file << "Iteracion\tPar\t\tImpar" << endl;
 	
 	for(int i = 0; i < number; i++) {
 		
@@ -22,16 +41,21 @@ void collatz(long int number)
 		
 		int contador_par = 0; 
 		int contador_impar = 0;
+		
+		if(isprime(n))
+			file << "-";
 	
 		while(n > 1)
 		{
 			//file << n << endl; //For print all numbers
 			
 			if ((n%2) == 1) {
-				n = 3*n+1;
+				
+				n = 3 * n+1;
 				contador_impar++;
 			}
 			else {
+				
 				n = n/2;
 				contador_par++;
 			}
@@ -45,7 +69,8 @@ void collatz(long int number)
 
 int main() {
 	
-	//Data analysis algorithm for the numbers of Collatz
+	//Algoritmo para analizar las iteraciones de la conjetura de Collatz
+	//Cuenta la cantidad de iteraciones con numeros pares e impares 
 	collatz(1000);
 	
 	return 0;
